5b88abe857376ba042c519db4e5001aeeff62cde
kauazica <kauajamariqueli150508@gmail.com>
2025-10-07 22:44:24 -0300

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"; // 👈 precisa estar na primeira linha!
 
+import "./styles/home.css";
 import { useState } from "react";
 
 export default function Home() {
@@ -11,7 +12,8 @@ export default function Home() {
         <img src="logo.png" alt="logo" />
         <h6>UtiliFácil</h6>
         <p>Suporte</p>
-        
+        <img className="social_i" src="insta.png" alt="social insta" />
+        <img className="social_t" src="tik-tok.png" alt="social tiktok" />
       </header>
     </main>
   );
